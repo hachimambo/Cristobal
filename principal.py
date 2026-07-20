72cc72173243f7741feaf53164d0d1abb74a439f
cristoblo <cris.henriquezr@duocuc.cl>
2026-07-20 15:30:01 -0400

    Add files via upload

---
+++ principal.py
@@ -0,0 +1,23 @@
+from os import system
+system("cls")
+from funciones import *
+peliculas = []
+
+while(True):
+    menu_principal()
+    opc = leer_opcion()
+    match opc:
+        case 1:
+            print("")
+        case 2:
+            print("")
+        case 3:
+            print("")
+        case 4:
+            print("")
+        case 5:
+            print("")
+        case 6:
+            print("")
+            break
+
